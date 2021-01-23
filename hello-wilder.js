45eb52dce211d0004a4e48c900256636d83dff08
ChristopheKleitz <ckleitz67@gmail.com>
2021-01-23 17:42:09 +0100

    Bases de JavaScript 1 - Découverte du langage

---
+++ hello-wilder.js
@@ -0,0 +1,7 @@
+const movieName = 'V pour Vendetta';
+const movieYear = 2006;
+const movieReal = 'James McTeigue';
+const message = 'V pour Vendetta est l\'adapation cinématographique de la bande dessinée homonyme écrite par ' +
+    'Alan Moore et illustrée par David Lloyd, qui vit le jour en 1981 dans le mensuel indépendant Warrior';
+
+alert(movieName + ' sorti en ' + movieYear + ' est un film de ' + movieReal + '. ' + message);
